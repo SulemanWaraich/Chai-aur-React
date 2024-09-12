9fc53d6a99fb96aedc9e51923a0691bfa9a2aa42
Muhammad Suleman <tech4you330@gmail.com>
2024-09-13 00:05:13 +0500

    update redux toolkit

--- 08Project-ReduxToolkit/src/App.jsx
+++ 08Project-ReduxToolkit/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/Todos'
 
 function App() {
  
   return (
     <>
       <h1>Chai</h1>
+      <AddTodo></AddTodo>
+      <Todos></Todos>
     </>
   )
 }
